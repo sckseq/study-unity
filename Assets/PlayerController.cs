6d408c4609ff2821ca934e57dd923b3171f149f3
= <=>
2023-11-22 00:06:57 +0900

    矢印押しても反応がない

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private Rigidbody _rigidbody;
+    private Transform _transform;
+    private Animator _animator;
+    private float _horizontal;
+    private float _vertical;
+    private Vector3 _velocity;
+    private float _speed = 2f;
+
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        _transform = GetComponent<Transform>();
+        _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+
+        _velocity = new Vector3(_horizontal, _rigidbody.velocity.y, _vertical).normalized;
+
+        Debug.Log(_velocity.magnitude);
+        if (_velocity.magnitude > 0.1f)
+        {
+            _animator.SetBool("walking", true);
+        }
+        else
+        {
+            _animator.SetBool("walking", false);
+        }
+
+        _rigidbody.velocity = _velocity * _speed;
     }
 }
